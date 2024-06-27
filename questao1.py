6d8ca2c9f7603dedcff6b9a30cb62e442b9dfe2c
Wisner Marques <wisner.marques@ufcat.edu.br>
2024-06-27 09:55:21 -0300

    avaliação prática - Lógica de Programação

---
+++ questao1.py
@@ -0,0 +1,17 @@
+'''
+Questão 1: Escreva uma função chamada classificar_idade que recebe um número inteiro representando a idade de uma pessoa e 
+retorna uma string que classifica a pessoa em uma das seguintes categorias:
+
+1- "Criança" se a idade for menor que 12 anos.
+2- "Adolescente" se a idade for entre 12 e 17 anos.
+3- "Adulto" se a idade for entre 18 e 64 anos.
+4- "Idoso" se a idade for 65 anos ou mais.
+
+
+# Teste a função
+print(classificar_idade(10))  # Saída esperada: Criança
+print(classificar_idade(15))  # Saída esperada: Adolescente
+print(classificar_idade(30))  # Saída esperada: Adulto
+print(classificar_idade(70))  # Saída esperada: Idoso
+
+'''
---
+++ questao2.py
@@ -0,0 +1,26 @@
+'''
+Questão 2: Implementar uma Calculadora com Loop
+Escreva uma função chamada calculadora que recebe três argumentos: dois números (a e b) e uma string representando a operação (operacao). 
+A função deve retornar o resultado da operação aplicada aos dois números. As operações suportadas são:
+
+    + para adição
+    - para subtração
+    * para multiplicação
+    / para divisão
+
+A função deve lidar com a divisão por zero e retornar uma mensagem apropriada nesse caso.
+
+Segue um trecho mostrando como deve ser implementada a função:
+    def calculadora(a, b, operacao):
+        if operacao == '+':
+            return a + b
+
+
+Na entrada dos números deve ser feito o tratamento de exceções, para que seja tratado os casos que não sejam digitados números.
+
+Os números digitados devem ser armazenados em uma lista e depois de encerrar, mostrar a lista, o maior número, o menor número e a média dos números.
+
+O programa deve ficar em loop, solicitando ao usuário os valores e a operação até que o usuário digite s para sair.
+
+'''
+
